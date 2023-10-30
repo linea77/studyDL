2ca544d4b305ba4d8dfd0f7f7fb73fd77a04aa32
linea77 <sunahan@korea.ac.kr>
2023-10-31 00:13:57 +0900

    20231030

---
+++ DLfromScratch1/ch1_matplotlib.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sin_plot(x):
+    y = np.sin(x)
+    plt.plot(x, y, label="sin")
+
+
+def cos_plot(x):
+    y = np.cos(x)
+    plt.plot(x, y, linestyle="--", label="cos")
+
+
+def draw_plot():
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    x = np.arange(0, 6, 0.1)
+
+    sin_plot(x)
+    cos_plot(x)
+    draw_plot()
